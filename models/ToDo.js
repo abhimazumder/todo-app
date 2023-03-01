1aa7daf718d7adf29a1ae054a264e3c1a16f60ae
Abhishek Mazumder <abhishek_mazumder@persistent.com>
2023-03-01 14:38:43 +0530

    Added update feature to todo list items

--- models/ToDo.js
+++ models/ToDo.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
 const ToDoSchema = mongoose.Schema({
+    _id : {
+        type : mongoose.Schema.Types.ObjectId
+    },
     todo : {
         type : String,
         required : true
--- routes/todo.js
+++ routes/todo.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const router = express.Router();
 const ToDo = require('../models/ToDo');
 
 router.post('/', async (req, res, next) => {
     const todo = new ToDo({
+        _id : new mongoose.Types.ObjectId(),
         todo : req.body.todo
     });
     await todo.save()
@@ -21,8 +23,45 @@ router.post('/', async (req, res, next) => {
     })
 });
 
-router.get('/:todo_id', async (req, res, next) => {
+router.get('/done/:todo_id', async (req, res, next) => {
     await ToDo.deleteOne({_id : req.params.todo_id})
+    .exec()
+    .then(result => {
+        console.log(result);
+        res.redirect('/');
+    })
+    .catch(error => {
+        console.log(error);
+        res.render('error', { error : {
+            status : 500,
+            message : "Internal Server Error"
+        }
+        });
+    })
+});
+
+router.get('/edit/:todo_id', async (req, res, next) => {
+    const doc = ToDo.find({_id : req.params.todo_id}).select('todo').exec()
+    .then(doc => {
+        console.log(doc);
+        res.render('edit', {
+            doc : {
+                _id : doc[0]._id,
+                todo : doc[0].todo
+            }
+        });
+    })    
+})
+
+router.post('/edit/:todo_id', async (req, res, next) => {
+    await ToDo.updateOne({
+        _id : req.params.todo_id
+    },
+    {
+        $set : {
+            todo : req.body.todo
+        }
+    })
     .exec()
     .then(result => {
         console.log(result);
